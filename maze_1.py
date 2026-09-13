4ea99c51985126a4f2b1469742e4cb8a704d73a8
samchouhan <sakshamchouhan8@gmail.com>
2026-09-13 19:29:16 +0530

    Refactor NODE and StackFrontier classes for improved readability and consistency

--- maze_1.py
+++ maze_1.py
@@ -1,27 +1,29 @@
 import sys
 
-class NODE():
-    def __init__(self,state,parent,action):
+
+class NODE:
+    def __init__(self, state, parent, action):
         self.action = action
         self.state = state
         self.parent = parent
-        
-#The path cost can be found later 
-        
-        
-class StackFrontier():
+
+
+# The path cost can be found later
+
+
+class StackFrontier:
     def __init__(self):
         self.frontier = []
-        
-    def add(self,node):
+
+    def add(self, node):
         self.frontier.append(node)
-        
-    def contains_state(self,state):
+
+    def contains_state(self, state):
         return any(node.state == state for node in self.frontier)
-    
+
     def empty(self):
         return len(self.frontier) == 0
-    
+
     def remove(self):
         if self.empty():
             raise Exception("empty frontier")
@@ -29,5 +31,3 @@ class StackFrontier():
             node = self.frontier[-1]
             self.frontier = self.frontier[:-1]
             return node
-        
-        
