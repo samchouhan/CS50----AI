33bcc67fe4ef0b25083da6d799d0259dcc2afebd
samchouhan <sakshamchouhan8@gmail.com>
2026-09-13 19:59:07 +0530

    Refactor MAZE class initialization for improved readability and consistency

--- maze_1.py
+++ maze_1.py
@@ -51,36 +51,38 @@ class QueueFrontier(StackFrontier):
             ]  # removed the first item of the list and returned it
             self.frontier = self.frontier[1:]
             return node
+
+
 class MAZE:
     def __init__(self, filename):
-         with open(filename) as f:
-             contents = f.read()
-             
-         if contents.count("A") != 1:
-                raise Exception("maze must have exactly one starting point")
-         if contents.count("B") != 1:
-                raise Exception("maze must have exactly one goal")
-            
-         contents = contents.splitlines()
-         self.height = len(contents)
-         self.width = max(len(line) for line in contents)
-        
-         self.walls = []
-         for i in range(self.height):
-             row = []
-             for j in range(self.width):
-                 try:
-                     if contents[i][j] == "A":
-                         self.start = (i, j)
-                         row.append(False)
-                     elif contents[i][j] == "B":
-                         self.goal = (i, j)
-                         row.append(False)
-                     elif contents[i][j] == " ":
-                         row.append(False)
-                     else:
-                         row.append(True)
-                 except IndexError:
-                     row.append(False)
-             self.walls.append(row)
-         self.solution = None
+        with open(filename) as f:
+            contents = f.read()
+
+        if contents.count("A") != 1:
+            raise Exception("maze must have exactly one starting point")
+        if contents.count("B") != 1:
+            raise Exception("maze must have exactly one goal")
+
+        contents = contents.splitlines()
+        self.height = len(contents)
+        self.width = max(len(line) for line in contents)
+
+        self.walls = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                try:
+                    if contents[i][j] == "A":
+                        self.start = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == "B":
+                        self.goal = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == " ":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(False)
+            self.walls.append(row)
+        self.solution = None
