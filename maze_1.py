53dc226252182247aaf692b17ba6fdf34be223cf
samchouhan <sakshamchouhan8@gmail.com>
2026-09-13 20:13:01 +0530

    Implement solve method in MAZE class to find a solution to the maze

--- maze_1.py
+++ maze_1.py
@@ -118,4 +118,48 @@ class MAZE:
             if 0 <= r < self.height and 0 <= c < self.width and not self.walls[r][c]:
                 result.append((action, (r, c)))
         return result
-    
+    def solve(self):
+         #Finds a solution to the maze, if one exists.
+         
+         #Keep track of number of states explored
+         self.num_explored = 0
+         
+         #Initialize the frontier to just the starting position
+         start = NODE(state=self.start, parent=None, action=None)
+         frontier = StackFrontier()
+         frontier.add(start)
+         
+         #Initialize an empty explored set
+         self.explored = set()
+         
+         #Keep looping until solution found
+         while True:
+             #If nothing left in frontier, then no path
+             if frontier.empty():
+                 raise Exception("no solution")
+             
+             #Choose a node from the frontier
+             node = frontier.remove()
+             self.num_explored += 1
+             
+             #If node is the goal, then we have a solution
+             if node.state == self.goal:
+                 actions = []
+                 cells = []
+                 while node.parent is not None:
+                     actions.append(node.action)
+                     cells.append(node.state)
+                     node = node.parent
+                 actions.reverse()
+                 cells.reverse()
+                 self.solution = (actions, cells)
+                 return
+             
+             #Mark node as explored
+             self.explored.add(node.state)
+             
+             #Add neighbors to frontier
+             for action, state in self.neighbors(node.state):
+                 if not frontier.contains_state(state) and state not in self.explored:
+                     child = NODE(state=state, parent=node, action=action)
+                     frontier.add(child)
