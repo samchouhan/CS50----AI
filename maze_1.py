c4f7be387268d04e9ce4d94144fba8e5b46a45ec
samchouhan <sakshamchouhan8@gmail.com>
2026-09-13 19:24:12 +0530

    Add NODE and StackFrontier classes to maze implementation

--- maze_1.py
+++ maze_1.py
@@ -1,1 +1,14 @@
 import sys
+
+class NODE():
+    def __init__(self,state,parent,action):
+        self.action = action
+        self.state = state
+        self.parent = parent
+        
+        
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+        
+        
